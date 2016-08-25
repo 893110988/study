3d59469951b22c1e11083be12fd1b061db298105
wmm893110988 <wangmm_1992@163.com>
2016-08-25 10:34:32 +0800

    动态代理

--- mycore/src/main/java/wmm/javaframe/study/designmodule/proxy/mydynamic/GoHomeProxy.java
+++ mycore/src/main/java/wmm/javaframe/study/designmodule/proxy/mydynamic/GoHomeProxy.java
@@ -7,11 +7,14 @@ import java.lang.reflect.Method;
  * Created by Administrator on 2016/8/24.
  */
 public class GoHomeProxy implements InvocationHandler{
+    private Object target;
+    public GoHomeProxy(Object target) {
+        this.target=target;
+    }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if( proxy instanceof GoHomeInterface && method.getName().equals("goHome"))
-            return "哈哈哈哈哈哈哈哈哈哈哈哈啊哈哈哈";
-
-        return method.invoke(GoHome.class.newInstance(),args);
+        /*if( target.getClass().isAssignableFrom(GoHome.class))
+            return "哈哈哈哈哈哈哈哈哈哈哈哈啊哈哈哈";*/
+        return method.invoke(target,args);
     }
 }
--- mycore/src/main/java/wmm/javaframe/study/designmodule/proxy/mydynamic/GoWork.java
+++ mycore/src/main/java/wmm/javaframe/study/designmodule/proxy/mydynamic/GoWork.java
@@ -3,10 +3,10 @@ package wmm.javaframe.study.designmodule.proxy.mydynamic;
 /**
  * Created by Administrator on 2016/8/24.
  */
-public class GoHome implements GoHomeInterface {
+public class GoWork implements GoHomeInterface {
 
     public String goHome() {
-        return "回家.........";
+        return "回家";
     }
 
     public String goWork() {
