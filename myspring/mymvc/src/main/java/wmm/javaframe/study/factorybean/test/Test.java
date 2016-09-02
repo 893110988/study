a28c539e667e596d74b2323033ccd068d92fe34b
wmm893110988 <wangmm_1992@163.com>
2016-09-02 17:59:27 +0800

    类加载

--- myspring/mymvc/src/main/java/wmm/javaframe/study/factorybean/test/Test.java
+++ myspring/mymvc/src/main/java/wmm/javaframe/study/factorybean/test/Test.java
@@ -13,6 +13,7 @@ public class Test {
 	public static void main(String[] args) {
 		Test t=new Test();
 		String xmlUrl=t.getClass().getResource("/").getFile().toString()+"/factorybean/factorybean.xml";
+		System.out.println(xmlUrl);
 		ApplicationContext factory=new ClassPathXmlApplicationContext("factorybean/factorybean.xml");
 		MyFactory me=(MyFactory)factory.getBean("myFactoryBean");
 		System.out.println("Son's name:"+me.getName());
